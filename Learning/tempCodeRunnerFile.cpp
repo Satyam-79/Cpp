855ecf20b15d014221e83b8090f0345497835c91
Satyam <satyamverma79@gmail.com>
2021-05-02 08:25:47 +0530

    '

--- Learning/tempCodeRunnerFile.cpp
+++ Learning/tempCodeRunnerFile.cpp
@@ -1,1 +1,13 @@
-#define MAX 100
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, c;
+    cout << "Enter two number: ";
+    cin >> a >> b;
+    c = a;
+    a = b;
+    b = c;
+    cout << "swaped numbers are: " << a << " " << b;
+    return 0;
+}
