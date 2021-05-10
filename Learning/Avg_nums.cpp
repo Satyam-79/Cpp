7e48faa3d427d5026551dff84f03e321e8041ce4
Satyam-79 <satyamverma79@gmail.com>
2021-05-10 12:54:26 +0530

    '

--- Learning/Avg_nums.cpp
+++ Learning/Avg_nums.cpp
@@ -2,6 +2,19 @@
 using namespace std;
 int main()
 {
-    
+    int num,sum=0,count=0;
+    float avg;
+    cout<<"\nEnter -1 to end";
+    cout<<"\nEnter the number : ";
+    cin>>num;
+    while (num !=-1)
+    {
+        count++;
+        sum=sum+num;
+        cout<<"\nEnter the number(-1 to stop) : ";
+        cin>>num;
+    }
+    avg=(float)sum/count;
+    cout<<"\nAverage : "<<avg;
     return 0;
 }
