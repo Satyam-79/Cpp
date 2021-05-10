b843bd22b13e28fa665b18e4722e5b14ae85d2c5
Satyam-79 <satyamverma79@gmail.com>
2021-05-10 20:12:32 +0530

    '

---
+++ Learning/nestedLoop_1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for ( i = 1; i <= 5; i++)
+    {
+        cout<<"\nPass "<<i<<" - ";
+        for ( j = 1; j <= 5; j++)
+        {
+            cout<<" "<<j;
+        }
+        
+    }
+    
+    
+    return 0;
+}
---
+++ Learning/nestedLoop_2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i,j;
+    for ( i = 1; i <= 5; i++)
+    {
+        cout<<"\n";
+        for ( j = 1; j <= 10; j++)
+        {
+            cout<<"* ";
+        }
+        
+    }
+    
+    
+    return 0;
+}
