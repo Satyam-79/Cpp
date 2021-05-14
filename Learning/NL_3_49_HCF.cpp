eeb81090e522b4a10491f0e3c7c52082f805b7ac
Satyam-79 <satyamverma79@gmail.com>
2021-05-14 19:32:13 +0530

    '

--- Learning/NL_3_49_HCF.cpp
+++ Learning/NL_3_49_HCF.cpp
@@ -2,25 +2,23 @@
 using namespace std;
 int main()
 {
-    int num1,num2,min,i,hcf;
-    cout<<"\nEnter first number : ";
-    cin>>num1;
-    cout<<"\nEnter second number : ";
-    cin>>num2;
-    if (num1<num2)
-    min=num1;
+    int num1, num2, min, i, hcf;
+    cout << "\nEnter first number : ";
+    cin >> num1;
+    cout << "\nEnter second number : ";
+    cin >> num2;
+    if (num1 < num2)
+        min = num1;
     else
-    min=num2;
-    for ( i = 1; i <= min; i++)
+        min = num2;
+    for (i = 1; i <= min; i++)
     {
-        if (num1%i==0&&num2%i==0)
+        if (num1 % i == 0 && num2 % i == 0)
         {
-            hcf=i;
+            hcf = i;
         }
-        
     }
-    cout<<"\nGCD of "<<num1<<" and "<<num2<<" is : "<<hcf;
-
+    cout << "\nGCD of " << num1 << " and " << num2 << " is : " << hcf;
 
     return 0;
 }
