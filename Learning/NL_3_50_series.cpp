729a45ba13a8d9c8614057b06d9b6cf304783c98
Satyam-79 <satyamverma79@gmail.com>
2021-05-15 20:18:56 +0530

    '

---
+++ Learning/NL_3_50_series.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    float sum = 0.0, i;
+    cout << "\nEnter the value of n : ";
+    cin >> n;
+    for (i = 1.0; i <= n; i++)
+    {
+        sum = sum + (1 / i);
+    }
+    cout << "\nThe sum of series 1/1 + 1/2.....1/" << n << " : " << sum;
+
+    return 0;
+}
