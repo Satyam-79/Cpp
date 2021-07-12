e8742cee59c6e69d34c0e33ff361239084180c3e
Satyam-79 <satyamverma79@gmail.com>
2021-07-13 00:43:39 +0530

    '

---
+++ Learning/parking_bill.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    char vehicle, temp;
+    int hour1, minute1, hour2, minute2, hour, minute, charges;
+    cout << "Enter the type of vehicle 'c' - car ; 'b' - bus/truck ; 's' - two wheeler : ";
+    cin >> vehicle;
+    // cout << "Enter the entering time of vehicle in HH : ";
+    // cin >> hour1;
+    // cout << "Enter the entering time of vehicle in MM : ";
+    // cin >> minute1;
+    // cout << "Enter the leaving time of vehicle in HH : ";
+    // cin >> hour2;
+    // cout << "Enter the leaving time of vehicle in MM : ";
+    // cin >> minute2;
+    cout << "Enter the entering time of vehicle in HH:MM  : ";
+    cin >> hour1 >> temp >> minute1;
+    cout << "Enter the leaving time of vehicle in HH:MM  : ";
+    cin >> hour2 >> temp >> minute2;
+    if (minute2 < minute1)
+    {
+        hour2 = hour2 - 1;
+        minute2 = minute2 + 60;
+    }
+    hour = hour2 - hour1;
+    minute = minute2 - minute1;
+
+    cout << "\nVehicle type : ";
+    vehicle == 'b' ? (cout << "Bus or Truck") : vehicle == 'c' ? (cout << "Car")
+                                                               : cout << "Two Wheeler";
+    cout << "\nTotal parked time : " << hour << ":" << minute << endl;
+    if (minute != 0)
+    {
+        hour = hour + 1;
+    }
+    if (vehicle == 'b')
+    {
+        if (hour <= 3)
+        {
+            charges = 20 * hour;
+        }
+        else
+            charges = (20 * 3) + (30 * (hour - 3));
+    }
+    if (vehicle == 'c')
+    {
+        if (hour <= 3)
+        {
+            charges = 10 * hour;
+        }
+        else
+            charges = (10 * 3) + (20 * (hour - 3));
+    }
+    if (vehicle == 's')
+    {
+        if (hour <= 3)
+        {
+            charges = 5 * hour;
+        }
+        else
+            charges = (5 * 3) + (10 * (hour - 3));
+    }
+
+    cout << "Parking Charges : " << charges;
+
+    return 0;
+}
--- Learning/time.cpp
+++ Learning/time.cpp
@@ -1,31 +1,13 @@
 #include <iostream>
+#include<time.h>
 using namespace std;
 int main()
 {
-    int a, y;
-    cin >> a;
-    int x[50];
-    for (int j = 0; j < a; j++)
-    {
-        cin >> x[j];
-    }
-    for (int j = 0; j < a; j++)
-    {
-        while (x[j] > 90)
-        {
-            y = y + 3;
-            x[j]--;
-        }
-        while (x[j] > 20)
-        {
-            y = y + 6;
-            x[j]--;
-        }
-        while (x[j] > 0)
-        {
-            y = y + 9;
-            x[j]--;
-        }
-        cout << y;
-    }
+    double hours,seconds;
+
+    
+
+
+
+    
 }
