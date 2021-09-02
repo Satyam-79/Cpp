c0f8d7d65aab305a9a00e7d0087e3f64c605e4bb
Satyam-79 <satyamverma79@gmail.com>
2021-09-02 19:25:04 +0530

    '

--- Learning/sort_student.cpp
+++ Learning/sort_student.cpp
@@ -6,22 +6,30 @@ int main()
     char name[5][10], temp[20];
     int n;
     cout << "\n Enter the number of student : ";
-    cin >> n, i, j;
+    cin >> n;
+    cin.ignore();
+
     for (int i = 0; i < n; i++)
     {
         cout << "\n Enter the name of the " << i + 1 << "th student : ";
-        cin.ignore();
         cin.getline(name[i], 100);
     }
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n - i - 1, j++)
+        for (int j = 0; j < n - i - 1; j++)
         {
             if (strcmp(name[j], name[j + 1]) > 0)
             {
-                /* code */
+                strcpy(temp, name[j]);
+                strcpy(name[j], name[j + 1]);
+                strcpy(name[j + 1], temp);
             }
         }
+    }
+    cout << "\n Names of students as stored in alphabetical order : ";
+    for (int i = 0; i < n; i++)
+    {
+        puts(name[i]);
     }
 
     return 0;
