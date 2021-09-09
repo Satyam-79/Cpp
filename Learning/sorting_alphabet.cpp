407a85fde2fe53cf1efc070cbf397deb7ab1a397
Satyam-79 <satyamverma79@gmail.com>
2021-09-09 12:46:22 +0530

    sort_bubble

---
+++ Learning/sorting_alphabet.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string.h>
+using namespace std;
+int main()
+{
+    char word[20], temp = 0;
+    int i = 0, n = 0;
+    cin.getline(word, 20);
+    n = strlen(word);
+    for (i = 0; i < n; i++)
+
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (word[j] > word[j + 1])
+            {
+                temp = word[j + 1];
+                word[j + 1] = word[j];
+                word[j] = temp;
+            }
+        }
+    }
+
+    puts(word);
+
+    return 0;
+}
