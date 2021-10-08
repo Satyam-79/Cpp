f3d95145538f25aa0355611ee6731782d15338a7
Satyam-79 <satyamverma79@gmail.com>
2021-10-08 15:27:52 +0530

    '

--- Learning/1.cpp
+++ Learning/1.cpp
@@ -1,1 +1,15 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+    int pow1 = 1;
+    while (pow1 < 4)
+    {
 
+        float b = pow(10, pow1);
+        cout << b << endl;
+        pow1++;
+    }
+    return 0;
+}
