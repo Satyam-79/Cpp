8725f63853dc67aa0b1a935dbab48dea8d0fd362
Satyam-79 <satyamverma79@gmail.com>
2021-10-09 11:45:37 +0530

    Binary search tree

---
+++ DSA/Binary_search_tree.cpp
@@ -0,0 +1,69 @@
+//  level order transversal binary tree
+#include <iostream>
+#include <queue>
+using namespace std;
+class node
+{
+public:
+    int data;
+    node *right;
+    node *left;
+
+public:
+    node(int val)
+    {
+        data = val;
+        right = NULL;
+        left = NULL;
+    }
+};
+void buildBST(node *&root, int val)
+{
+    node *n = new node(val);
+    node *temp = root;
+    if (val < temp->data && temp->left == NULL)
+    {
+        temp->left = n;
+        return;
+    }
+    if (val > temp->data && temp->right == NULL)
+    {
+        temp->right = n;
+        return;
+    }
+
+    if (val < temp->data)
+    {
+        buildBST(temp->left, val);
+    }
+    else if (val > temp->data)
+    {
+        buildBST(temp->right, val);
+    }
+}
+void inOrder(node *&a)
+{
+    if (a == NULL)
+    {
+        return;
+    }
+    inOrder(a->left);
+    cout << a->data << "\t";
+    inOrder(a->right);
+}
+
+int main()
+{
+    node *a = new node(1);
+    buildBST(a, 1);
+    buildBST(a, 3);
+    buildBST(a, 4);
+    buildBST(a, 2);
+    buildBST(a, 7);
+    buildBST(a, 2);
+    buildBST(a, 9);
+    buildBST(a, 5);
+    inOrder(a);
+
+    return 0;
+}
