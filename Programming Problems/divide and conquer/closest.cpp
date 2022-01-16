39355b41ec1a08c1bbcd0dbf4d396b8c8c0f7267
Satyam-79 <satyamverma79@gmail.com>
2022-01-16 16:18:22 +0530

    pp

--- Programming Problems/divide and conquer/closest.cpp
+++ Programming Problems/divide and conquer/closest.cpp
@@ -17,7 +17,7 @@ using std::string;
 using std::vector;
 // using namespace std;
 
-double distance(pair<int, int> p1, pair<int, int> p2)
+double distance(pair<long long, long long> p1, pair<long long, long long> p2)
 {
   double x = pow((p1.first - p2.first), 2);
   double y = pow((p1.second - p2.second), 2);
@@ -25,7 +25,7 @@ double distance(pair<int, int> p1, pair<int, int> p2)
   return sqrt(x + y);
 }
 
-double nativeDist(vector<pair<int, int>> &points, int beg, int end)
+double nativeDist(vector<pair<long long, long long>> &points, long long beg, long long end)
 {
   double min = __FLT_MAX__;
   for (size_t i = beg; i <= end; i++)
@@ -40,20 +40,20 @@ double nativeDist(vector<pair<int, int>> &points, int beg, int end)
   }
   return min;
 }
-double minimal_distance(vector<pair<int, int>> &xSorted, vector<pair<int, int>> &ySorted, int beg, int end)
+double minimal_distance(vector<pair<long long, long long>> &xSorted, vector<pair<long long, long long>> &ySorted, long long beg, long long end)
 {
   double d1, d2, d, d_min = __FLT_MAX__;
   auto mid = (beg + end) / 2;
-  pair<int, int> midPoint = xSorted[mid];
+  pair<long long, long long> midPoint = xSorted[mid];
 
   if ((end - beg + 1) <= 3)
   {
     return nativeDist(xSorted, beg, end);
   }
 
-  vector<pair<int, int>> ySorted_left;
+  vector<pair<long long, long long>> ySorted_left;
   ySorted_left.reserve(end - beg + 1);
-  vector<pair<int, int>> ySorted_right;
+  vector<pair<long long, long long>> ySorted_right;
   ySorted_right.reserve(end - beg + 1);
 
   for (size_t i = beg; i <= end; i++)
@@ -72,7 +72,7 @@ double minimal_distance(vector<pair<int, int>> &xSorted, vector<pair<int, int>> 
   d2 = minimal_distance(xSorted, ySorted_right, mid + 1, end);
   d_min = min(d1, d2);
 
-  vector<pair<int, int>> strip;
+  vector<pair<long long, long long>> strip;
   strip.reserve(end - beg + 1);
 
   for (size_t i = 0; i < ySorted.size(); i++)
@@ -99,19 +99,19 @@ int main()
   cin.tie(NULL);
   ios_base::sync_with_stdio(false);
 
-  int n, x, y;
+  long long n, x, y;
   std::cin >> n;
 
-  vector<pair<int, int>> points;
+  vector<pair<long long, long long>> points;
   points.reserve(n);
   for (size_t i = 0; i < n; i++)
   {
     cin >> x >> y;
     points.push_back(make_pair(x, y));
   }
-  vector<pair<int, int>> xSorted;
+  vector<pair<long long, long long>> xSorted;
   xSorted.reserve(n);
-  vector<pair<int, int>> ySorted;
+  vector<pair<long long, long long>> ySorted;
   ySorted.reserve(n);
 
   for (size_t i = 0; i < n; i++)
@@ -120,9 +120,9 @@ int main()
     ySorted.push_back(make_pair(points[i].first, points[i].second));
   }
 
-  sort(xSorted.begin(), xSorted.end(), [](pair<int, int> &a, pair<int, int> &b)
+  sort(xSorted.begin(), xSorted.end(), [](pair<long long, long long> &a, pair<long long, long long> &b)
        { return a.first < b.first; });
-  sort(ySorted.begin(), ySorted.end(), [](pair<int, int> &a, pair<int, int> &b)
+  sort(ySorted.begin(), ySorted.end(), [](pair<long long, long long> &a, pair<long long, long long> &b)
        { return a.second < b.second; });
 
   // for (auto &&i : xSorted)
